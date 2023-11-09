0d33759ade99fda79b634c738f7108b3c21718ee
Manav Patel <manavpatel@Manavs-MacBook-Pro.local>
2023-11-09 12:30:27 -0600

    initial

---
+++ api/Database.cs
@@ -0,0 +1,21 @@
+namespace api
+{
+    public class Database
+    {
+        private string host{get; set;}
+        private string database{get; set;}
+        private string username{get; set;}
+        private string port{get; set;}
+        private string password{get; set;}
+        public string cs{get; set;}
+
+        public Database(){
+            host = "dfkpczjgmpvkugnb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+            database = "aovklha05u21jv78";
+            username= "xoax93ec0p6qflzx";
+            port="3306";
+            password="nn1xb0wqzjjcjwqew";
+            cs = $"server={host};user={username};database={database};port={port};password={password}";
+        }
+    }
+}
