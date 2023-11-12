307894e5e28702f5b268a0f4d68af249f2856743
Manav Patel <manavpatel@Manavs-MacBook-Pro.local>
2023-11-12 17:12:36 -0600

    addcors

--- api/Program.cs
+++ api/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddCors(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+app.UseCors("OpenPolicy")
 
 var app = builder.Build();
 
