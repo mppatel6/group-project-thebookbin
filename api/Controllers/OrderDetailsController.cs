a708945ce4ec181fef22cd49dbadb8b78e548759
Manav Patel <manavpatel@Manavs-MacBook-Pro.local>
2023-11-27 21:24:59 -0600

    ODcontroller

---
+++ api/Controllers/OrderDetailsController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using api.Models;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderDetailsController : ControllerBase
+    {
+        // GET: api/OrderDetails
+        [HttpGet]
+        public List<OrderDetails> Get()
+        {
+            OrderDetailsUtility readOD = new OrderDetailsUtility();
+            return readOD.ReadOD();
+        }
+
+        // GET: api/OrderDetails/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/OrderDetails
+        [HttpPost]
+        public void Post([FromBody] OrderDetails value)
+        {
+            OrderDetailsUtility addOD = new OrderDetailsUtility();
+            addOD.AddOrderDetails(value);
+        }
+
+        // PUT: api/OrderDetails/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] OrderDetails value)
+        {
+            OrderDetailsUtility editOD = new OrderDetailsUtility();
+            editOD.EditOrderDetails(value);
+        }
+
+        // DELETE: api/OrderDetails/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
