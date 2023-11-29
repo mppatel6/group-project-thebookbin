8e52f1ee772e9eb1c427875af3faea0536134fba
Manav Patel <manavpatel@Manavs-MacBook-Pro.local>
2023-11-29 14:40:34 -0600

    env file

--- api/Database.cs
+++ api/Database.cs
@@ -1,21 +1,22 @@
+using DotNetEnv;
+
 namespace api
 {
     public class Database
     {
-        private string host{get; set;}
-        private string database{get; set;}
-        private string username{get; set;}
-        private string port{get; set;}
-        private string password{get; set;}
         public string cs{get; set;}
 
-        public Database(){
-            host = "dfkpczjgmpvkugnb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-            database = "aovklha05u21jv78";
-            username= "xoax93ec0p6qflzx";
-            port="3306";
-            password="n1xb0wqzjjcjwqew";
-            cs = $"server={host};user={username};database={database};port={port};password={password}";
+        public Database()
+        {
+            DotNetEnv.Env.Load();
+
+            var Host = Environment.GetEnvironmentVariable("DB_HOST");
+            var DatabaseName = Environment.GetEnvironmentVariable("DB_DATABASE");
+            var Username = Environment.GetEnvironmentVariable("DB_USERNAME");
+            var Port = Environment.GetEnvironmentVariable("DB_PORT");
+            var Password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+            cs = $"server={Host};user={Username};database={DatabaseName};port={Port};password={Password}";
         }
     }
 }
